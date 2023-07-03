3732c4be94e38b31f1d4f04e5f2ca996dc678689
Luciana Pereira <luci.lv14@gmail.com>
2023-07-03 01:08:22 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -12,11 +12,11 @@
 
 #include "../include/minishell.h"
 
-int main(int argc, char **argv, char **envp)
+
+int	main(int argc, char **argv, char **envp)
 {
 	t_data data;
 	t_shell shell;
-	// int fd;
 	char *line;
 
 	(void)argc;
@@ -24,18 +24,22 @@ int main(int argc, char **argv, char **envp)
 	init_data(&data, &shell, envp);
 	if (!data.envp)
 		exit(EXIT_FAILURE);
+	shell.input = NULL;
 	while (1)
 	{
 		shell.quit = 0;
-		free(shell.input);
 		init_signal();
-		ft_putstr_fd("minishell>", 2);
-		// fd = open(shell.input, O_RDONLY);
-		line = get_next_line(0);
-		if (!line)
-			free_memory(&data, shell.input);
-		else
-			init_parser(line, &data, &shell);
+		line = readline("minishell> ");
+		if (line == NULL)
+		{
+			printf("\n");
+			break;
+		}
+		if (ft_strlen(line) > 0)
+			add_history(line);
+		init_parser(line, &data, &shell);
+		free(line);
 	}
+	rl_clear_history();
 	return (0);
 }
