ce7137031a65c3123e950c90576b9e0f23d01656
Luciana Pereira <luci.lv14@gmail.com>
2023-07-05 16:19:41 -0300

    Update parser_utils.c

--- src/parsing/parser_utils.c
+++ src/parsing/parser_utils.c
@@ -6,12 +6,14 @@
 /*   By: lucperei <lucperei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 02:11:16 by lucperei          #+#    #+#             */
-/*   Updated: 2023/06/25 03:13:09 by lucperei         ###   ########.fr       */
+/*   Updated: 2023/07/05 04:33:27 by lucperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+// A função copia um caractere de escape do ponteiro src para o ponteiro dst, 
+// substituindo as aspas simples (') pelo caractere de aspas duplas ("), ou vice-versa. Basicamente, ela escapa o caractere.
 void escape_char(char **dst, char **src)
 {
     char ctr;
@@ -26,6 +28,9 @@ void escape_char(char **dst, char **src)
     *((*dst)++) = ctr;
 }
 
+// A função copia os caracteres do ponteiro src para o ponteiro dst, até encontrar o caractere 
+// ptr. Se o caractere ptr for aspas duplas ("), ela lida com caracteres de escape (precedidos 
+// por \) e inverte a ação de escapar, enquanto copia os caracteres para dst.
 void copy_ptr(char **src, char **dst, char ptr)
 {
     int count;
@@ -44,6 +49,9 @@ void copy_ptr(char **src, char **dst, char ptr)
     }
 }
 
+// A função a percorre o str e incrementa o valor de index a cada caractere encontrado, 
+// até encontrar o caractere ptr. Se o caractere ptr for aspas duplas ("), 
+// ela lida com caracteres de escape (precedidos por \) e ajusta o valor de index de acordo.
 void ptr_size(char **str, int *index, char ptr)
 {
     int count;
