a2df7fb173c671129fb93babd5cf3164dcb03397
Luciana Pereira <luci.lv14@gmail.com>
2023-07-05 16:12:52 -0300

    Update pwd.c

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -6,12 +6,13 @@
 /*   By: lucperei <lucperei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 21:40:05 by lucperei          #+#    #+#             */
-/*   Updated: 2023/06/25 05:18:05 by lucperei         ###   ########.fr       */
+/*   Updated: 2023/07/05 18:58:13 by lucperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+// Imprimir o diretório atual (path working directory) na saída padrão.
 void ft_pwd(t_data *data)
 {
     t_shell shell;
