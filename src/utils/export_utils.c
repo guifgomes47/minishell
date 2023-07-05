737e5732c3040bf49b43916c1f9a606156fa3c76
Luciana Pereira <luci.lv14@gmail.com>
2023-07-05 16:23:19 -0300

    Update export_utils.c

--- src/utils/export_utils.c
+++ src/utils/export_utils.c
@@ -6,28 +6,34 @@
 /*   By: lucperei <lucperei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/24 13:24:48 by lucperei          #+#    #+#             */
-/*   Updated: 2023/06/25 03:12:01 by lucperei         ###   ########.fr       */
+/*   Updated: 2023/07/05 03:34:19 by lucperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+// Verifica se input atende aos critérios para ser exportada como uma variável de ambiente. 
 int verify_export(char *input)
 {
     int index;
 
     index = 0;
+	// Verifica se e um digito, se for retorna 0 indicando que não pode ser exportada
     if (ft_isdigit(input[index]))
         return (0);
+		// Intera para localizar o caracter '='
     while (input[index] && input[index] != '=')
     {
+		// verifica se cada caractere é alfanumérico ou underscore ('_'), se for retorna 0 indicando que não pode ser exportada
         if (!ft_isalnum(input[index]) && input[index] != '_')
             return (0);
         index++;
     }
-    return (1);
+	// Retorna 1, para informar que atende aos critérios e pode ser exportada.
+    return (1); 
 }
 
+// Função exibe as variáveis de ambiente no formato esperado
 int show_export(char **env)
 {
     int x;
