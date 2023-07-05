6c255366c10d286fc688023afe2277bac50f4116
Luciana Pereira <luci.lv14@gmail.com>
2023-07-05 16:10:58 -0300

    Update exit.c

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: lucperei <lucperei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 22:07:37 by lucperei          #+#    #+#             */
-/*   Updated: 2023/06/25 05:36:45 by lucperei         ###   ########.fr       */
+/*   Updated: 2023/07/05 19:51:04 by lucperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,17 @@ void exit_pipe(t_data *data)
     exit(EXIT_SUCCESS);
 }
 
+// Define o status e exibe mensagem de erro
 void error_message(char *str, int status)
 {
     t_shell shell;
 
     shell.status = status;
+	perror(str);
     // ft_putstr_fd(str, 2);
-    perror(str);
 }
 
+// Verifica se e um numero
 int is_number(char *str)
 {
     int index;
@@ -48,6 +50,8 @@ int is_number(char *str)
     return (1);
 }
 
+// Função lida com o comando "exit", ela verifica os argumentos fornecidos ao comando 
+// e define o status de saída do shell com base nos argumentos. 
 void ft_exit(char **command, t_data *data)
 {
     t_shell shell;
@@ -57,6 +61,7 @@ void ft_exit(char **command, t_data *data)
     {
         if (is_number(command[1]))
         {
+			// Se houver mais de um argumento, uma mensagem de erro é exibida. 
             if (command[2])
                 return (error_message("Error exit: too many arguments\n", 2));
             shell.status = ft_atoi(command[1]);
