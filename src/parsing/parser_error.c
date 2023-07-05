0dda2981a9f3d4d96d18817e01f97106b311a66a
Luciana Pereira <luci.lv14@gmail.com>
2023-07-05 16:18:31 -0300

    Update parser_error.c

--- src/parsing/parser_error.c
+++ src/parsing/parser_error.c
@@ -6,12 +6,13 @@
 /*   By: lucperei <lucperei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 14:37:20 by lucperei          #+#    #+#             */
-/*   Updated: 2023/06/25 04:34:38 by lucperei         ###   ########.fr       */
+/*   Updated: 2023/07/05 04:59:50 by lucperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+// responsável por pular ou "ignorar" as aspas em uma str
 void skip_quotes(char *str, int *index)
 {
     char quote;
@@ -26,6 +27,7 @@ void skip_quotes(char *str, int *index)
     }
 }
 
+// verifica se há erros ou problemas de redirecionamento em uma string.
 int redir_error(char *str, int *index, char c)
 {
     int count;
@@ -37,14 +39,15 @@ int redir_error(char *str, int *index, char c)
         (*index)++;
     }
     if ((c == '>' && count > 2) || (c == '<' && count > 1))
-        return (1);
+        return (1);//indicando um erro de redirecionamento
     while (str[(*index)] == ' ')
         (*index)++;
     if (!str[(*index)])
-        return (1);
+        return (1); 
     return (0);
 }
 
+// função principal responsável pelo processamento de erros em um comando
 int parser_error(char *str)
 {
     int index;
