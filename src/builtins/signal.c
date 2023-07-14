307c9b12b4b2ca1216765a8ff3e604c7cc8e2fe6
Luciana Pereira <luci.lv14@gmail.com>
2023-07-14 17:38:10 -0300

    Update signal.c

--- src/builtins/signal.c
+++ src/builtins/signal.c
@@ -6,7 +6,7 @@
 /*   By: lucperei <lucperei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 22:18:14 by lucperei          #+#    #+#             */
-/*   Updated: 2023/07/05 18:51:26 by lucperei         ###   ########.fr       */
+/*   Updated: 2023/07/07 01:31:29 by lucperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,45 @@
 // Esta função lida om os sinais enviados para o programa. 
 void handle_signal(int signal)
 {
-	t_shell shell;
-	
-	// teclas Ctrl+C
     if (signal == SIGINT)
-    {
-		// define o status para indicar que o programa foi encerrado devido a um sinal SIGINT.
-        shell.status = 130;
-        if (shell.input)
-            ft_putstr_fd("\nminishell>", 2);
-			// Libera memoria se tiver algum registro de entrada
-        if (shell.input)
-            free(shell.input);
-        shell.input = ft_strdup("\0");
-    }
-    else if (signal == SIGQUIT)
-        write(2, "\b\b \b\b", 6); // usado para "apagar" a linha atual.
+	{
+        printf("\n");
+        rl_on_new_line();
+        rl_replace_line("", 0);
+        rl_redisplay();
+    }	
+	// t_shell shell;
+	// // teclas Ctrl+C
+    // if (signal == SIGINT)
+    // {
+	// 	// define o status para indicar que o programa foi encerrado devido a um sinal SIGINT.
+    //     shell.status = 130;
+    //     if (shell.input)
+    //         ft_putstr_fd("\nminishell>", 2);
+	// 		// Libera memoria se tiver algum registro de entrada
+    //     if (shell.input)
+    //         free(shell.input);
+    //     shell.input = ft_strdup("\0");
+    // }
+    // else if (signal == SIGQUIT)
+    //     write(2, "\b\b \b\b", 6); // usado para "apagar" a linha atual.
 }
 
 //  Inicializar o tratamento de sinais 
 void init_signal(void)
 {
+    // if (signal(SIGINT, handle_signal) == SIG_ERR)
+    //     exit(EXIT_FAILURE);
+    // else if (signal(SIGQUIT, handle_signal) == SIG_ERR)
+    //     exit(EXIT_FAILURE);
     if (signal(SIGINT, handle_signal) == SIG_ERR)
         exit(EXIT_FAILURE);
-    else if (signal(SIGQUIT, handle_signal) == SIG_ERR)
+    else if (signal(SIGQUIT, SIG_IGN) == SIG_ERR)
         exit(EXIT_FAILURE);
 }
+
+// void init_signal(void)
+// {
+// 	signal(SIGINT, handle_signal);
+//     signal(SIGQUIT, SIG_IGN);
+// }
